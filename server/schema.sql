1863f4793a7b1e1d170510e4d29a942847da4e72
Kevin Coyner <rustybear@gmail.com>
2017-08-05 17:02:22 -0400

    kMac.home: Finished designing schema and succesfully ran schema.sql.

--- server/schema.sql
+++ server/schema.sql
@@ -30,14 +30,32 @@ DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
   mid INTEGER NOT NULL AUTO_INCREMENT,
   body MEDIUMTEXT NULL DEFAULT NULL,
-  createdAt TIMESTAMP NULL DEFAULT NOW(),
-  updatedAt TIMESTAMP NULL DEFAULT NOW(),
+  createdAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   uid_users INTEGER NULL DEFAULT NULL,
   rid_rooms INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (mid),
+  PRIMARY KEY (mid)
 );
 
 /*
   createdAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   updatedAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   */
+DROP TABLE IF EXISTS `users_rooms`;
+
+CREATE TABLE `users_rooms` (
+  `urid` INTEGER NOT NULL AUTO_INCREMENT,
+  `uid_users` INTEGER NULL DEFAULT NULL,
+  `rid_rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`urid`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (uid_users) REFERENCES `users` (`uid`);
+ALTER TABLE `messages` ADD FOREIGN KEY (rid_rooms) REFERENCES `rooms` (`rid`);
+ALTER TABLE `users_rooms` ADD FOREIGN KEY (uid_users) REFERENCES `users` (`uid`);
+ALTER TABLE `users_rooms` ADD FOREIGN KEY (rid_rooms) REFERENCES `rooms` (`rid`);
+
