7cf5ba9703e67e2d0c796f8738f0770c3a0bb2e2
Kevin Coyner <rustybear@gmail.com>
2017-08-06 15:07:07 -0400

    doodle: GET works (mostly), POST almost there.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,13 +5,35 @@ module.exports = {
     get: function (req, res) {
       //console.log(req.url);
       models.messages.get(function(results){
+        //res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({results: results}));
       });
 
     }, // a function which handles a get request for all messages
 
 
-    post: function (req, res) {} // a function which handles posting a message to the database
+
+    post: function (req, res) {
+      var message = '';
+      var headers = {};
+      req.on('data', function(chunk) {
+        message += chunk;
+        //console.log('message: ', message);
+      }).on('end', function() {
+
+        var test = JSON.parse(message);
+        console.log(test);
+        // var test = JSON.parse(message);
+        // console.log(test);
+        // models.messages.post(function(message) {
+        // //  lines 31-33
+        // });
+        headers['Content-Type'] = 'application/json';
+        res.writeHead(200, headers);
+        res.end('test');
+      }); // a function which handles posting a message to the database
+    }
   },
 
   users: {
@@ -19,5 +41,6 @@ module.exports = {
     get: function (req, res) {},
     post: function (req, res) {}
   }
+
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -5,11 +5,11 @@ module.exports = {
   messages: {
     get: function(callback) {
       //db.connect();
-      var queryString = 'select m.mid as objectId, u.username, m.text, r.roomname, m.createdAt, m.updatedAt from messages as m, users as u, rooms as r';
-      var queryArgs = [];
-      db.query(queryString, queryArgs, function(err, results) {
+      var queryString = 'SELECT messages.mid AS objectId, users.username, rooms.roomname, messages.text, messages.createdAt as created, messages.updatedAt FROM users INNER JOIN (rooms INNER JOIN messages ON rooms.rid = messages.rid_rooms) ON users.uid = messages.uid_users order by ?? desc limit ?';
+      var queryArgs = ['created', 10];
+      var myQuery = db.query(queryString, queryArgs, function(err, results) {
         if (err) {
-          console.log('error:', results);
+          console.log('Query error:', results);
           //db.end();
         } else {
           //db.end();
@@ -20,7 +20,12 @@ module.exports = {
     }, // a function which produces all the messages
 
 
-    post: function() {} // a function which can be used to insert a message into the database
+    post: function(message) {
+      // this is done on a callback
+       console.log(JSON.parse(message));
+      //  sql insert
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
@@ -32,7 +37,7 @@ module.exports = {
 
 
 
-// SELECT messages.mid AS objectId, users.username, rooms.roomname, messages.body
-// FROM users INNER JOIN (rooms INNER JOIN messages ON rooms.[rid] = messages.rid_rooms) ON users.[uid] = messages.uid_users;
+// SELECT messages.mid AS objectId, users.username, rooms.roomname, messages.body FROM users INNER JOIN (rooms INNER JOIN messages ON rooms.[rid] = messages.rid_rooms) ON users.[uid] = messages.uid_users;
 
+      // var queryString = 'select m.mid as objectId, u.username, m.text, r.roomname, m.createdAt, m.updatedAt from messages as m, users as u, rooms as r';
 
