1e951aec0692e806c4efea266e479619ff878ce4
Kevin Coyner <rustybear@gmail.com>
2017-08-09 00:00:07 -0400

    kMac.home: Using sequelize, successfully set up db and structure.

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,54 @@
 var mysql = require('mysql');
-
+var Sequelize = require('sequelize');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+
+var db = new Sequelize('mysql://kevinroot:codeDoggy@hr-rpt01.cybvxu5jsrlj.us-east-1.rds.amazonaws.com/chat');
+
+db
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+const Users = db.define('users', {
+  //uid: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  username: Sequelize.STRING
+});
+
+const Messages = db.define('messages', {
+  //mid: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  text: Sequelize.TEXT,
+  createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+  updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
+});
+
+const Rooms = db.define('rooms', {
+  //rid: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  roomname: Sequelize.STRING
+});
+
+Users.hasMany(Messages);
+Rooms.hasMany(Messages);
+
+// Users.sync().then(() => {
+//   // Table created
+//   return Users.create({
+//     username: 'John'
+//   });
+// });
+Users.sync();
+
+Rooms.sync();
+
+Messages.sync();
+
+Messages.findAll().
+then(text => {console.log(text)});
 
 var dbConnection = mysql.createConnection({
   user: 'kevinroot',
@@ -12,3 +58,4 @@ var dbConnection = mysql.createConnection({
 });
 
 module.exports = dbConnection;
+
